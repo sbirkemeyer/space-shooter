4ce06067f042e9af74bd21f3822721bf3bee1f95
Simon Birkemeyer <simon.birkemeyer@r132-16-w10.SCHULE-ISERV>
2017-11-02 13:58:26 +0100

    first commit

---
+++ Assets/Done/Scripts/GameController.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+	public GUIText moneyText;
+
+
+	private bool restart;
+	private bool gameOver;
+	private int score;
+	private int money;
+
+	void Start ()
+	{
+		score = 0;
+		money = 0; 
+		gameOver = false;
+		restart = false;
+		gameOverText.text = "";
+		restartText.text = "";
+		UpdateScore ();
+		UpdateMoney ();
+		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update()
+	{
+		if (restart) 
+		{
+			if (Input.GetKeyDown (KeyCode.R)) 
+			{
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+
+			if (gameOver) 
+			{
+				restartText.text = "Press 'R' to Restart";
+				restart = true;
+				break;
+			}
+		}
+	}
+
+	public void AddScore(int newScoreValue)
+	{
+		score += newScoreValue ;
+		UpdateScore ();
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+	public void AddMoney(int newMoneyValue)
+	{
+		money += newMoneyValue ;
+		UpdateMoney ();
+	}
+
+	void UpdateMoney()
+	{
+		moneyText.text = "Money: " + money;
+	}
+
+	public void GameOver()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+	}
+}
---
+++ Assets/Done/Scripts/Mover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Mover : MonoBehaviour 
+{
+	public float speed;
+
+	void Start () 
+	{
+		GetComponent<Rigidbody> ().velocity = transform.forward * speed;
+	}
+}
